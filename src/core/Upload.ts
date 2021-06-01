2511f58259e824ebee6daf56d950ba8baf04d2db
nishajakhar <ruhaniroy26396@gmail.com>
2021-06-01 22:38:07 +0530

    Extension Accepted : PDF

--- src/core/Upload.ts
+++ src/core/Upload.ts
@@ -2,20 +2,15 @@ import multer from 'multer'
 import path = require('path')
 
 const fileFilter = (req: any, file: any, cb: any) => {
-  if (
-    file.mimetype === 'image/jpeg' ||
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg'
-  )
-    cb(null, true)
-  else return cb(new Error('Only .png, .jpg and .jpeg format allowed!'))
+  if (file.mimetype === 'application/pdf') cb(null, true)
+  else return cb(new Error('Only .pdf format allowed!'))
 }
 const storage = multer.diskStorage({
   destination: function (req: any, file: any, cb: any) {
     cb(null, path.join(__dirname, '../../', '/public/Documents'))
   },
   filename: function (req: any, file: any, cb: any) {
-    cb(null, 'Pic-' + Date.now())
+    cb(null, 'File-' + Date.now())
   },
 })
 export const upload = multer({
